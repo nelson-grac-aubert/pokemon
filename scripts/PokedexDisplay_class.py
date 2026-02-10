3d7ae77b2ca772994d58863e259fd752f3d21b0a
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-10 10:17:53 +0100

    feat : create full kanto pokedex, improve pokedex display

--- scripts/PokedexDisplay_class.py
+++ scripts/PokedexDisplay_class.py
@@ -3,3 +3,14 @@ from Pokedex_class import Pokedex
 class PokedexDisplay : 
     def __init__(self, pokedex : Pokedex) : 
         self.__pokedex = pokedex
+
+    def get_pokedex(self) : 
+        return self.__pokedex
+    def set_pokedex(self, new_pokedex) :
+        self.__pokedex = new_pokedex
+
+    def draw_all_pokemons(self) : 
+        for pokemon in self.get_pokedex.get_pokemons() : 
+            pass
+
+    
---
+++ scripts/pokedex_menu.py
@@ -0,0 +1,11 @@
+import pygame
+from Pokemon_class import Pokemon 
+from Pokedex_class import Pokedex
+from PokedexDisplay_class import PokedexDisplay
+from PixelButton_class import PixelButton
+from all_pokemons import kanto_pokemons
+
+kanto_pokedex = Pokedex(kanto_pokemons)
+
+
+
