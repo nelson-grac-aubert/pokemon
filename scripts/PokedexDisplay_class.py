672ed1ab115712baf51cacc45d69a294e7744bac
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-10 09:54:16 +0100

    feat : create poledex logic and pokedex display classes

---
+++ scripts/PokedexDisplay_class.py
@@ -0,0 +1,5 @@
+from Pokedex_class import Pokedex
+
+class PokedexDisplay : 
+    def __init__(self, pokedex : Pokedex) : 
+        self.__pokedex = pokedex
---
+++ scripts/Pokedex_class.py
@@ -0,0 +1,19 @@
+from all_pokemons import * 
+
+class Pokedex : 
+    def __init__(self) : 
+        self.__pokemons = []
+
+    def get_pokemons(self) : 
+        return self.__pokemons
+    def set_pokemons(self, new_pokemons) :
+        self.__pokemons = new_pokemons
+
+    def add_pokemon(self, new_pokemon : Pokemon) : 
+        """
+        Add a pokemon to the Pokedex
+        
+        :param new_pokemon: The pokemon added to the pokedex
+        :type new_pokemon: Pokemon
+        """
+        self.get_pokemons().append(new_pokemon)
