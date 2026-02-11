6e3395072f79f30f3de86a1ce8af46ba73a305e9
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-11 11:39:44 +0100

    fix : correct typo

--- scripts/classes/PokedexButton_class.py
+++ scripts/classes/PokedexButton_class.py
@@ -1,7 +1,7 @@
 import pygame
 from scripts.logic.assets_management import load_image
 
-class PokemonButton:
+class PokedexButton:
     def __init__(self) : 
         self.__img = load_image("assets/images/pokedex.png").convert_alpha()
         self.__img = pygame.transform.scale(self.img, (140, 120))  # adapte la taille 
