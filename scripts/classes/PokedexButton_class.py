79fc6f744fc453d5db16d943e2918e8baf173924
nelson-grac-aubert <nelson.grac-aubert@laplateforme.io>
2026-02-11 11:27:38 +0100

    refactor : start extracting pokedex button into its own class

---
+++ scripts/classes/PokedexButton_class.py
@@ -0,0 +1,12 @@
+import pygame
+from scripts.logic.assets_management import load_image
+
+class PokemonButton:
+    def __init__(self) : 
+        self.__img = load_image("assets/images/pokedex.png").convert_alpha()
+        self.__img = pygame.transform.scale(self.img, (140, 120))  # adapte la taille 
+        self.__img_rect = self.img.get_rect()
+        self.__img_rect.topright = (780, 20)  # position en haut à droite
+        self.__hover_scale = 1.0
+        self.__target_scale = 1.0
+        self.__button_click_cooldown = 0
